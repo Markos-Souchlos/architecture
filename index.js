9b1daed8b72386f1ab8aaf28f25fc804cadd65fd
Markos-Souchlos <markosouxlos@gmail.com>
2024-12-29 02:58:24 +0200

    Added footer map and go-to-top button

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-
-
+const topBtn = document.querySelector('#top');
 const navbar = document.querySelector('#main-nav');
 const mobileNav = document.querySelector('#mobile-nav');
+const logo = document.querySelector('#logo-img');
 
 export function underline(num) {
-    document.querySelector(`#main-nav li:nth-child(${num}) a`).style.borderBottom = '3px solid red'
+    document.querySelector(`#main-nav li:nth-child(${num}) a`).style.borderBottom = '3px solid #eaa33a'
 }
 
 export function increaseNav() {
@@ -12,8 +12,10 @@ export function increaseNav() {
     window.addEventListener('scroll', () => {
         if (window.scrollY > 130) {
             navbar.style.height = '60px';
+            logo.style.height = '60px';
         } else {
             navbar.style.height = '80px'; 
+            logo.style.height = '80px';
         }
     });
 
@@ -31,10 +33,20 @@ export function resize() {
 
     window.addEventListener("resize", () => {
         const windowWidth = window.innerWidth;
-        console.log("Window width changed to:", windowWidth);
         if (windowWidth>650) {
             mobileNav.classList.remove('visible');
         }
     })
+}
+
+export function goToTop() {
+    document.querySelector('#top').addEventListener('click', () => {
+        window.scrollTo({top:0, behavior:'smooth'})
+    })
+}
+
+export function getYear() {
+    const year = new Date().getFullYear();
+    document.querySelector('#year').textContent = year;
 }
 
