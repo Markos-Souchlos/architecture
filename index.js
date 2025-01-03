aa6d51355341a373188316b275b1ced95af2ac52
Markos-Souchlos <markosouxlos@gmail.com>
2025-01-03 23:37:12 +0200

    Added services sectioin and background-attachment fixed image

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const navbar = document.querySelector('#main-nav');
 const mobileNav = document.querySelector('#mobile-nav');
 const logo = document.querySelector('#logo-img');
 
+
+export function navigationEn() {
+    // console.log(document.querySelector('#main-nav.en'));
+
+    document.querySelector('#main-nav.en').innerHTML = `
+    <ul>
+        <li id="li-logo"><a href="/en/home.html" id="logo"><img src="../public/assets/icons/88d728b9-073a-4b3e-bbb2-1975ab491d05.jpeg" alt="logo" id="logo-img" ></a></li>
+
+        <li><a href="/en/home.html">HOME</a></li>
+        <li><a href="/en/about.html">ABOUT</a></li>
+        <li><a href="/en/projects.html">PROJECTS</a></li>
+        <li><a href="/en/gallery.html">GALLERY</a></li>
+        <li><a href="/en/contact.html">CONTACT</a></li>
+        <li><button class="change-lang"><img src="https://img.icons8.com/fluency/48/usa-circular.png" alt="USA flag" /></button></li>
+        <li id="sandwitch"><button><img width="96" height="96" src="https://img.icons8.com/plumpy/96/menu--v5.png" alt="menu--v5"/></button></li>
+    </ul>
+    `;
+}
+
 export function underline(num) {
     document.querySelector(`#main-nav li:nth-child(${num}) a`).style.borderBottom = '3px solid #eaa33a'
 }
@@ -64,7 +83,7 @@ export function getYear() {
 export function changeLanguage(lang, page) {
     document.querySelectorAll('.change-lang').forEach(element => {
         element.addEventListener('click', () => {
-            console.log('index.js works');
+            // console.log('index.js works');
             window.location.href = `/${lang}/${page}.html`;
         })
     });
